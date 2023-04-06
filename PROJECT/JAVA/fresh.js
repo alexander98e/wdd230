12a78c4585fabb42ba68ec42c93e94751af4036b
alexander98e <echave98alexander@gmail.com>
2023-04-06 14:53:21 -0600

    final

--- PROJECT/JAVA/fresh.js
+++ PROJECT/JAVA/fresh.js
@@ -16,3 +16,10 @@ $(function() {
  
  });
 
+ function testVariable() {
+  var strText = document.getElementById("textone").value;          
+  var strText1 = document.getElementById("textTWO").value;
+  var result = strText + ' ' + strText1;
+  document.getElementById('spanResult').textContent = result;
+   
+}
